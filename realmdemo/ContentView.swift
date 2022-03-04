284bdb887e2b5878b5c9e689beee407554581d11
박힘찬 <himchan05@naver.com>
2022-03-04 10:05:27 +0900

    Initial Commit

---
+++ realmdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  realmdemo
+//
+//  Created by 박힘찬 on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ realmdemo/realmdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  realmdemoApp.swift
+//  realmdemo
+//
+//  Created by 박힘찬 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct realmdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

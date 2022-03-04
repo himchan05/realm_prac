3b82bf23136dc539cf774b4ec7e44f8f5ce79495
박힘찬 <himchan05@naver.com>
2022-03-04 17:35:56 +0900

    realm test

--- realmdemo/ContentView.swift
+++ realmdemo/ContentView.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
-
+import RealmSwift
+class DateRealm: Object {
+    @objc dynamic var year = ""
+    @objc dynamic var month = ""
+    @objc dynamic var day = ""
+}
 struct ContentView: View {
+    init() {
+        let dateSelected = DateRealm()
+        dateSelected.year = "1997"
+        dateSelected.month = "05"
+        dateSelected.day = "23"
+        
+        let realm = try! Realm()
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+
+        try! realm.write { // Realm에 데이터 저장
+            realm.add(dateSelected)
+        }
+    }
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .background(Color.red)
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
